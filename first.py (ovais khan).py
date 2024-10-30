5fe2dabea7a55a99e210413983ebf641f52c49e6
Ovais-Qarni <ovais5618@gmail.com>
2024-10-30 20:28:20 +0500

    Assignment 1
    We are created our first assignment

---
+++ first.py (ovais khan).py
@@ -0,0 +1,38 @@
+number1=8
+number2=16
+number3=23
+
+# calculate sum
+sum=number1+number2+number3
+print("sum:",sum)
+
+# calculate difference
+diffference=number1-number2-number3
+print("difference:",diffference)
+
+# calculate product
+product=number1*number2*number3
+print("product:",product)
+
+# calculate average
+average=(number1+number2+number3)
+print("average:",average)
+#area of triangle
+breathe=9
+height=6
+area=0.5*breathe*height
+print("triangle area",area)
+#area of square
+side=6
+area=side*side
+print("square area",area)
+#area of circle
+p1=6.14
+radius=5
+area=p1*radius*radius
+print("circle area",area)
+#rectangle area
+length=19
+width=6
+area=length*width
+print("rectangle area",area)
